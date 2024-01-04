2223aa118d81b7dc127052dea7d312a8baf1c97e
Billy <billymarles31@gmail.com>
2024-01-04 10:43:48 -0500

    first commit

--- AppBack/app.js
+++ AppBack/app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', require('./routes/api'));
 
-const PORT = process.env.PORT || 27017;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
